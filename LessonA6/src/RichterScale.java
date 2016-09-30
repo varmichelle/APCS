5a19dbe7048aab9bee238f4311cda69c5e08ab6c
Michelle Li <michellecarolynclark@gmail.com>
2016-09-30 14:52:55 -0700

    lab a8 richter scale

---
+++ LessonA6/src/RichterScale.java
@@ -0,0 +1,24 @@
+/**
+ * Class RichterScale - outputs damage to buildings given magnitude
+ * @author Michelle Li
+ * Period 1
+ *
+ */
+
+public class RichterScale {
+
+	
+	/**
+	 * method to determine and print appropriate message for earthquake damage
+	 * @param magnitude double for the magnitude of the earthquake
+	 */
+	public static void effect(double magnitude) {
+		
+		if (magnitude >= 8) System.out.println("Most structures fall");
+		else if (magnitude >= 7) System.out.println("Many buildings destroyed");
+		else if (magnitude >= 6) System.out.println("Many buildings considerably damaged, some collapse");
+		else if (magnitude >= 4.5) System.out.println("Damage to poorly constructed buildings");
+		
+	}
+
+}

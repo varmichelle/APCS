3f161e3ba66dbba9be3bb423820d9dcb61e0801b
Michelle Li <michellecarolynclark@gmail.com>
2016-09-30 15:27:38 -0700

    lab A.8.2 elevator

---
+++ LessonA8/src/Elevator.java
@@ -0,0 +1,34 @@
+/**
+ * Class Elevator - simulates an elevator
+ * @author: Michelle Li
+ * Period: 1
+ */
+
+
+import java.util.*;
+
+public class Elevator {
+
+	private Scanner scan = new Scanner(System.in);
+	
+	private final int MAX_FLOORS = 20;
+	
+	/**
+	 * Prints appropriate message given floor number
+	 *  
+	 */
+	public void simulate() {
+		System.out.println("Floor: ");
+		if (!scan.hasNextInt()) System.out.println("Error: Not an integer");
+		else {
+			int floor = scan.nextInt();
+			if (floor == 13) System.out.println("Error: There is no thirteenth floor");
+			else if (floor > MAX_FLOORS || floor <= 0) 
+				System.out.println("Error: The floor must be between 1 and " + MAX_FLOORS);
+			else if (floor > 13) System.out.println("Thank you, I will take you to the actual floor " + (floor - 1));
+			else System.out.println("Thank you, I will take you to the actual floor " + floor);
+			
+		}
+	}
+
+}

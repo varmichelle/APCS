1bd570493804ec66fcdcb34aaf54b9a1ef72286c
Michelle Li <michellecarolynclark@gmail.com>
2016-10-10 15:31:53 -0700

    write seq part 1

---
+++ LessonA8/src/WriteSequence.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Michelle Li
+ * Period 1
+ * Class WriteSequence - prints a symmetric sequence of n numbers
+ *
+ */
+public class WriteSequence {
+
+	public void write() {
+		
+	}
+
+}

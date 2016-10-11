76b6ed19c1a6911baccef0a840a16588efed3658
Michelle Li <michellecarolynclark@gmail.com>
2016-10-11 14:14:06 -0700

    bug fix for digit match

--- LessonA8/src/DigitMatch.java
+++ LessonA8/src/DigitMatch.java
@@ -15,7 +15,7 @@ public class DigitMatch {
 	 */
 	public int countMatch(int a, int b) {
 		
-		if (a <= 0 && b <= 0) return 0;
+		if (a <= 0 || b <= 0) return 0;
 		else {
 			if (a % 10 == b % 10) return 1 + countMatch(a/10, b/10);
 		}

eb39ac4026f56ef5c94bc18b9eb88f30f760fa50
Michelle Li <michellecarolynclark@gmail.com>
2016-11-13 11:16:38 -0800

    employee lab 11

--- LessonA11/src/Employee.java
+++ LessonA11/src/Employee.java
@@ -7,4 +7,22 @@
 
 public class Employee {
 
+	private String name;
+	
+	/**
+	 * Constructor to initialize instance variable name
+	 * @param name - name of employee
+	 */
+	public Employee(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Getter method for name
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+	
 }

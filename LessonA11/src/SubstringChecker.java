85d68690e6f6ad968ca514baaf996c986aea357a
Michelle Li <michellecarolynclark@gmail.com>
2016-11-13 12:14:44 -0800

    substring checker lab 11

--- LessonA11/src/SubstringChecker.java
+++ LessonA11/src/SubstringChecker.java
@@ -1,6 +1,29 @@
+/**
+ * 
+ * @author Michelle Li
+ * Period 1
+ *
+ */
 
-public class SubstringChecker {
+public class SubstringChecker implements Checker {
 
+	private String substring;
 	
+	/**
+	 * Constructor to initialize instance variables
+	 * @param substring - substring to check for
+	 */
+	public SubstringChecker(String substring) {
+		this.substring = substring;
+	}
+	
+	/**
+	 * Method accept to determine whether or not the checker will accept input text
+	 * @param text - text to check
+	 * @return if checker accepts text
+	 */
+	public boolean accept(String text) {
+		return text.contains(substring);
+	}
 
 }

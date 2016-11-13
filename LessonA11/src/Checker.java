3be44d1e9b1d166cb332edc71daabd758a5acd73
Michelle Li <michellecarolynclark@gmail.com>
2016-11-13 12:04:57 -0800

    checker lab 11

---
+++ LessonA11/src/Checker.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Michelle Li
+ * Period 1
+ *
+ */
+
+public interface Checker {
+
+	/**
+	 * Method accept to determine if checker accepts text or not
+	 * @param text - a string to consider for acceptance
+	 * @return - true if checker accepts text, false otherwise
+	 */
+	boolean accept(String text);
+	
+}

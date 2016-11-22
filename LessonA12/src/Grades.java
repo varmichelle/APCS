40e2090744b0957ebeac1578e74bfe18d9f8f380
Michelle Li <michellecarolynclark@gmail.com>
2016-11-22 07:47:05 -0800

    added comments to grades

--- LessonA12/src/Grades.java
+++ LessonA12/src/Grades.java
@@ -15,14 +15,21 @@ public class Grades {
 	private String message;
 	Scanner in = new Scanner(System.in);
 
+  /**
+   * Takes in grades input and calculates gpa
+   */
 	public void getGradesAndCalculateGPA() {
+	  // reset everything
 	  numGrades = 0;
 	  gpa = 0;
 	  hasF = 0;
 	  System.out.print("Enter your grades: ");
+	  // take in input
 	  String s = in.next();
+	  // break if s isn't a legal grade character
 		while ((s.toLowerCase().charAt(0) >= 'a' && s.toLowerCase().charAt(0) <= 'd') || s.toLowerCase().charAt(0) == 'f') {
 			numGrades++;
+			// add appropriate value to gpa
 			if (s.toLowerCase().equals("a")) gpa += 4;
 			else if (s.toLowerCase().equals("b")) gpa += 3;
 			else if (s.toLowerCase().equals("c")) gpa += 2;
@@ -30,8 +37,10 @@ public class Grades {
 			else if (s.toLowerCase().equals("f")) hasF = 1;
 			s = in.next();
 		}
+		// compute and print gpa
 	  gpa /= numGrades;
     System.out.printf("GPA = %.2f\n", gpa);
+    // set the appropriate message
 		if (numGrades < 4) {
 			message = "Ineligible, taking less than 4 classes";
 		} else {
@@ -45,6 +54,9 @@ public class Grades {
 		}
 	}
 	
+	/**
+	 * Prints message
+	 */
 	public void printMessage() {
 		System.out.println(message);
 	}

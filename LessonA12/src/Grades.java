1a4a6ba1a814156d766af205f6ae1692e2183bfa
Michelle Li <michellecarolynclark@gmail.com>
2016-11-22 15:21:06 -0800

    fixed indentation

--- LessonA12/src/Grades.java
+++ LessonA12/src/Grades.java
@@ -1,3 +1,4 @@
+
 /**
  * 
  * @author Michelle Li
@@ -8,26 +9,27 @@
 import java.util.*;
 
 public class Grades {
-	
+
 	private int numGrades = 0;
 	private double gpa = 0;
 	private int hasF = 0; // 0 = F, 1 = T
 	private String message;
 	Scanner in = new Scanner(System.in);
 
-  /**
-   * Takes in grades input and calculates gpa
-   */
+	/**
+	 * Takes in grades input and calculates gpa
+	 */
 	public void getGradesAndCalculateGPA() {
-	  // reset everything
-	  numGrades = 0;
-	  gpa = 0;
-	  hasF = 0;
-	  System.out.print("Enter your grades: ");
-	  // take in input
-	  String s = in.next();
-	  // break if s isn't a legal grade character
-		while ((s.toLowerCase().charAt(0) >= 'a' && s.toLowerCase().charAt(0) <= 'd') || s.toLowerCase().charAt(0) == 'f') {
+		// reset everything
+		numGrades = 0;
+		gpa = 0;
+		hasF = 0;
+		System.out.print("Enter your grades: ");
+		// take in input
+		String s = in.next();
+		// break if s isn't a legal grade character
+		while ((s.toLowerCase().charAt(0) >= 'a' && s.toLowerCase().charAt(0) <= 'd')
+				|| s.toLowerCase().charAt(0) == 'f') {
 			numGrades++;
 			// add appropriate value to gpa
 			if (s.toLowerCase().equals("a")) gpa += 4;
@@ -38,27 +40,27 @@ public class Grades {
 			s = in.next();
 		}
 		// compute and print gpa
-	  gpa /= numGrades;
-    System.out.printf("GPA = %.2f\n", gpa);
-    // set the appropriate message
+		gpa /= numGrades;
+		System.out.printf("GPA = %.2f\n", gpa);
+		// set the appropriate message
 		if (numGrades < 4) {
 			message = "Ineligible, taking less than 4 classes";
 		} else {
-			 if (gpa < 2) {
-				 if (hasF == 1) message = "Ineligible, gpa below 2.0 and has F grade";
-				 else message = "Ineligible, gpa below 2.0";
-			 } else {
-				 if (hasF == 1) message = "Ineligible, gpa above 2.0 but has F grade";
-				 else message = "Eligible";
-			 }
+			if (gpa < 2) {
+				if (hasF == 1) message = "Ineligible, gpa below 2.0 and has F grade";
+				else message = "Ineligible, gpa below 2.0";
+			} else {
+				if (hasF == 1) message = "Ineligible, gpa above 2.0 but has F grade";
+				else message = "Eligible";
+			}
 		}
 	}
-	
+
 	/**
 	 * Prints message
 	 */
 	public void printMessage() {
 		System.out.println(message);
 	}
-	
+
 }

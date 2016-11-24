9cd239e0e3f1e55f265da482c028d154281efaf3
Michelle Li <michellecarolynclark@gmail.com>
2016-11-24 12:32:02 -0800

    chocolate pt 1

---
+++ LessonA12/src/Chocolate.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Michelle Li
+ * Period 1
+ * This class calculates how many bricks needed to reach goal
+ *
+ */
+
+public class Chocolate {
+
+	public boolean canMakeBricks(int small, int big, int goal) {
+		if (small + big * 5 >= goal) return true;
+		else return false;
+	}
+	
+	public int numberOfSmallBricksNeeded(int small, int big, int goal) {
+		
+	}
+	
+}

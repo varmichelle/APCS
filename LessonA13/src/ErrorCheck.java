8b41c3bd57740befb0c5ee78fe93f8a34d41a9b3
Michelle Li <michellecarolynclark@gmail.com>
2017-01-04 15:00:59 -0800

    checking account

--- LessonA13/src/ErrorCheck.java
+++ LessonA13/src/ErrorCheck.java
@@ -1,6 +1,29 @@
 
 public class ErrorCheck {
 
+	private double myBalance;
+	private int myAccountNumber;
 	
+	public ErrorCheck() {
+		myBalance = 0;
+		myAccountNumber = 0;
+	}
+	
+	public ErrorCheck(double initialBalance, int acctNum) {
+		myBalance = initialBalance;
+		myAccountNumber = acctNum;
+	}
+	
+	public double getBalance() {
+		return myBalance;
+	}
+	
+	public void deposit(double amount) {
+		myBalance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		myBalance -= amount;
+	}
 
 }

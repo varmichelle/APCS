07d319061cec4720c5ce8acd8ccf87db5162d33f
Michelle Li <michellecarolynclark@gmail.com>
2017-01-04 16:07:02 -0800

    stop words constructor

--- LessonA13/src/RemoveStopWords.java
+++ LessonA13/src/RemoveStopWords.java
@@ -1,4 +1,34 @@
+/**
+ * Class RemoveStopWords to remove specified words from text file
+ * @author Michelle Li
+ * Period 1
+ *
+ */
 
 public class RemoveStopWords {
 
+	private String inFile, outFile, stopList;
+	private int maxLineLength;
+	
+	/**
+	 * Constructor to initialize file names and max line length
+	 * @param inFile - name of input file
+	 * @param outFile - name of output file
+	 * @param maxLineLength - max number of characters in a line
+	 * @param stopList - name of stop words list file
+	 */
+	public RemoveStopWords(String inFile, String outFile, int maxLineLength, String stopList) {
+		this.inFile = inFile;
+		this.outFile = outFile;
+		this.maxLineLength = maxLineLength;
+		this.stopList = stopList;
+	}
+	
+	/**
+	 * Remove stop words from input file into output file
+	 */
+	public void removeStopWords() {
+		
+	}
+	
 }

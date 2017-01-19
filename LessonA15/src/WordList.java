92b11e879abe075baecfbd23655824d3b295b1ca
Michelle Li <michellecarolynclark@gmail.com>
2017-01-19 07:45:26 -0800

    no iterator in wordlist

--- LessonA15/src/WordList.java
+++ LessonA15/src/WordList.java
@@ -30,9 +30,11 @@ public class WordList extends ArrayList<String> {
 	 * @param len - length to check for
 	 */
 	public void removeWordsOfLength(int len) {
-		Iterator i = list.iterator();
-		while (i.hasNext()) {
-			if (((String) i.next()).length() == len) i.remove();
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).length() == len) {
+				list.remove(i);
+				i--;
+			}
 		}
 	}
 	

9ca90b2328b378461efd46f439c0a14bb557d6a2
Michelle Li <michellecarolynclark@gmail.com>
2017-01-30 16:09:27 -0800

    student and student2

---
+++ LessonA16/src/Student2.java
@@ -0,0 +1,124 @@
+/**
+ * Class Student to simulate a student
+ * @author Michelle Li
+ * @period 1
+ *
+ */
+
+import java.util.*;
+
+public class Student2 {
+	
+	// instance variables
+	private String name;
+	private final int NUM_TESTS = 3;
+	private ArrayList<Integer> testScores = new ArrayList<Integer>();
+	
+	/**
+	 * Constructor to initialize name to empty String and testScores to 0
+	 */
+	public Student2() {
+		name = "";
+		for (int i = 0; i < NUM_TESTS; i++) {
+			testScores.add(0);
+		}
+	}
+	
+	/**
+	 * Constructor to initialize name to given value and test scores to 0
+	 * @param a - name of student
+	 */
+	public Student2(String a) {
+		name = a;
+		for (int i = 0; i < NUM_TESTS; i++) {
+			testScores.add(0);
+		}
+	}
+	
+	/**
+	 * Constructor to initialize name to given value and test scores to given values
+	 * @param a - name of student
+	 * @param nums - test scores of student
+	 */
+	public Student2(String a, ArrayList<Integer> nums) {
+		name = a;
+		for (int i = 0; i < NUM_TESTS; i++) {
+			testScores.add(nums.get(i));
+		}
+	}
+	
+	/**
+	 * Constructor to initialize Student to given Student
+	 * @param other - other Student
+	 */
+	public Student2(Student other) {
+		name = other.getName();
+		for (int i = 0; i < NUM_TESTS; i++) {
+			testScores.add(other.getScore(i));
+		}
+	}
+	
+	/**
+	 * Set the name of the student to given value
+	 * @param a - name of student
+	 */
+	public void setName(String a) {
+		name = a;
+	}
+	
+	/**
+	 * Get the name of the student
+	 * @return name - name of student
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Set the test scores of the student
+	 * @param test - index of test score to set
+	 * @param score - the score to set
+	 */
+	public void setScore(int test, int score) {
+		testScores.set(test, score);
+	}
+	
+	/**
+	 * Get the test score of the student
+	 * @param test - index of test score to get
+	 * @return testScores[test] - the test score requested
+	 */
+	public int getScore(int test) {
+		return testScores.get(test);
+	}
+	
+	/**
+	 * Get the average test score
+	 * @return avg - avg test score
+	 */
+	public int getAverage() {
+		int avg = 0;
+		for (int i = 0; i < NUM_TESTS; i++) avg += testScores.get(i);
+		avg /= NUM_TESTS;
+		return avg;
+	}
+	
+	/**
+	 * Get the highest test score
+	 * @return max - highest score
+	 */
+	public int getHighScore() {
+		int max = 0;
+		for (int i = 0; i < NUM_TESTS; i++) max = Math.max(max, testScores.get(i));
+		return max;
+	}
+	
+	/**
+	 * Get the data of the Student in string form
+	 * @return data in string form
+	 */
+	public String toString() {
+		return "";
+	}
+	
+}

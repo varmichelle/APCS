bc2be280eb50fc8ddde9555996f799e6df2df6c7
Michelle Li <michellecarolynclark@gmail.com>
2017-02-01 10:29:56 -0800

    goldbach

--- LessonA16/src/Goldbach.java
+++ LessonA16/src/Goldbach.java
@@ -3,25 +3,24 @@
  * primes. Goldbach's conjecture says that this is always possible if N is even
  * and greater than 2. When N is odd, these are called prime pairs.
  * 
- * @author Your name 
- *  period ##
+ * @author Michelle Li 
+ * Period 1
  */
+
 public class Goldbach {
+	
 	private Eratosthenes e;
 	private final int N;
 
 	/**
 	 * Constructor creates the list of primes using the class Eratosthenes and
 	 * initializes N to the passed in value of n
-	 * 
-	 * @param n
-	 *            - number to be written as a sum of two primes
+	 * @param n - number to be written as a sum of two primes
 	 */
 	public Goldbach(int n) {
-		//Your code goes here
+		e = new Eratosthenes(n);
+		N = n;
 	}
-
-	
 
 	/**
 	 * Returns a string that includes all the prime pairs that add up to N
@@ -29,6 +28,22 @@ public class Goldbach {
 	 * @return string with the all the prime pairs that add up to N
 	 */
 	public String sumOfTwoPrimes() {
-		//Your code goes here
+		String[] answers = new String[N+1];
+		int[] primes = e.listOfPrimes();
+		for (int p1 = 0; p1 < primes.length; p1++) {
+			for (int p2 = p1; p2 < primes.length; p2++) {
+				int sum = primes[p1] + primes[p2];
+				if (answers[sum].equals("")) {
+					String s = sum + " = " + primes[p1] + " + " + primes[p2];
+					answers[sum] = s;
+				}
+			}
+		}
+		String s = "";
+		for (int i = 2; i <= N; i++) {
+			if (answers[i].equals("")) s += i + " not expressible as sum of two primes";
+			else s += answers[i];
+		}
+		return s;
 	}
 }

3dc02f8a9c9132f0691452f245e148fb5da85881
Michelle Li <michellecarolynclark@gmail.com>
2017-02-01 10:18:29 -0800

    era

--- LessonA16/src/Eratosthenes.java
+++ LessonA16/src/Eratosthenes.java
@@ -10,46 +10,67 @@ import java.util.*;
 public class Eratosthenes {
 	
 	// Instance variables
-	private int[] primes;
+	private int[] numbers;
 	
 	/**
 	 * Constructor to initialize array of primes using the Sieve of Eratosthenes
 	 * @param n - all primes are less than or equal to n
 	 */
 	public Eratosthenes (int n) {
-		ArrayList<Integer> numbers = new ArrayList<Integer>();
-		// fill with numbers from 2 to n initially
 		for (int i = 2; i <= n; i++) {
-			numbers.add(i);
+			numbers[i-2] = i;
 		}
-		for (int i = 2; i <= n; i++) {
-			
+		for (int i = 0; i <= n-2; i++) {
+			if (numbers[i] != -1) {
+				for (int j = i + 1; j <= n-2; j++) {
+					if (numbers[j] % numbers[i] == 0) {
+						numbers[j] = -1;
+					}
+				}
+			}
 		}
 	}
 	
 	/** Count the number of primes
 	 * @return number of primes less than or equal to n
 	 */
-	public int countPrimes()
-	{
-		//Your code goes here
+	public int countPrimes() {
+		int count = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] != -1) count++;
+		}
+		return count;
 	}
 	
 	/**
 	 * returns a list of primes less than or equal to n
-	 * @return
+	 * @return list of primes
 	 */
-	public int[] listOfPrimes()
-	{
-		//Your code goes here
+	public int[] listOfPrimes() {
+		int[] primes = new int[countPrimes()];
+		int index = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] != -1) {
+				primes[index] = numbers[i];
+				index++;
+			}
+		}
+		return primes;
 	}
 	
 	/**
 	 * Returns a string containing the prime numbers and the number of prime numbers
 	 * in the list
 	 */
-	public String toString()
-	{
-		//Your code goes here
+	public String toString(){
+		String s = "";
+		int[] primes = listOfPrimes();
+		for (int i = 0; i < primes.length; i++) {
+			s += primes[i];
+			if (i != primes.length - 1) s += " ";
+			else s += "\n";
+		}
+		s += "Number of primes less than or equal to " + (numbers.length+1) + " = " + countPrimes();
+		return s;
 	}
 }

2c42309da987f7453f333ccf8bd8bc79c81495cf
Michelle Li <michellecarolynclark@gmail.com>
2017-01-31 19:37:13 -0800

    goldbach, eratosthenes

--- LessonA16/src/Eratosthenes.java
+++ LessonA16/src/Eratosthenes.java
@@ -1,4 +1,55 @@
+/**
+ * Determines the primes less than or equal to n using the Sieve of Eratosthenes
+ * @author Michelle Li
+ * Period #1
+ *
+ */
+
+import java.util.*;
 
 public class Eratosthenes {
-
+	
+	// Instance variables
+	private int[] primes;
+	
+	/**
+	 * Constructor to initialize array of primes using the Sieve of Eratosthenes
+	 * @param n - all primes are less than or equal to n
+	 */
+	public Eratosthenes (int n) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		// fill with numbers from 2 to n initially
+		for (int i = 2; i <= n; i++) {
+			numbers.add(i);
+		}
+		for (int i = 2; i <= n; i++) {
+			
+		}
+	}
+	
+	/** Count the number of primes
+	 * @return number of primes less than or equal to n
+	 */
+	public int countPrimes()
+	{
+		//Your code goes here
+	}
+	
+	/**
+	 * returns a list of primes less than or equal to n
+	 * @return
+	 */
+	public int[] listOfPrimes()
+	{
+		//Your code goes here
+	}
+	
+	/**
+	 * Returns a string containing the prime numbers and the number of prime numbers
+	 * in the list
+	 */
+	public String toString()
+	{
+		//Your code goes here
+	}
 }
---
+++ LessonA16/src/Goldbach.java
@@ -0,0 +1,34 @@
+/**
+ * Computes all primes less than N and tries to express N as a sum of two
+ * primes. Goldbach's conjecture says that this is always possible if N is even
+ * and greater than 2. When N is odd, these are called prime pairs.
+ * 
+ * @author Your name 
+ *  period ##
+ */
+public class Goldbach {
+	private Eratosthenes e;
+	private final int N;
+
+	/**
+	 * Constructor creates the list of primes using the class Eratosthenes and
+	 * initializes N to the passed in value of n
+	 * 
+	 * @param n
+	 *            - number to be written as a sum of two primes
+	 */
+	public Goldbach(int n) {
+		//Your code goes here
+	}
+
+	
+
+	/**
+	 * Returns a string that includes all the prime pairs that add up to N
+	 * 
+	 * @return string with the all the prime pairs that add up to N
+	 */
+	public String sumOfTwoPrimes() {
+		//Your code goes here
+	}
+}

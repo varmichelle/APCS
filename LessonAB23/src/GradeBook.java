34c70dc8e6670366102ccb70a110fd0f1dc7a38c
Michelle Li <michellecarolynclark@gmail.com>
2017-02-16 16:49:04 -0800

    matrix manipulator pt 1

--- LessonAB23/src/GradeBook.java
+++ LessonAB23/src/GradeBook.java
@@ -1,4 +1,63 @@
+/**
+ * @author Michelle Li
+ * @period #1
+ * Class GradeBook - stores grades of students
+ */
 
 public class GradeBook {
 
+	private String courseName;
+	private int[][] grades;
+	
+	public GradeBook(String name, int[][] gradesArray) {
+		courseName = name;
+		for (int row = 0; row < gradesArray.length; row++) {
+			for (int col = 0; col < gradesArray[0].length; col++) {
+				grades[row][col] = gradesArray[row][col];
+			}
+		}
+	}
+	
+	public int getMinimum() {
+		int min = 999999;
+		for (int row = 0; row < grades.length; row++) {
+			for (int col = 0; col < grades[0].length; col++) {
+				min = Math.min(min, grades[row][col]);
+			}
+		}
+		return min;
+	}
+	
+	public int getMaximum() {
+		int max = 0;
+		for (int row = 0; row < grades.length; row++) {
+			for (int col = 0; col < grades[0].length; col++) {
+				max = Math.max(max, grades[row][col]);
+			}
+		}
+		return max;
+	}
+	
+	public double getAverage(int[] setOfGrades) {
+		double avg = 0;
+		for (int i = 0; i < setOfGrades.length; i++) {
+			avg += setOfGrades[i];
+		}
+		avg /= setOfGrades.length;
+		return avg;
+	}
+	
+	public void outputBarChart() {
+		
+	}
+	
+	public void outputGrades() {
+		
+	}
+	
+	public void processGrades() {
+		
+	}
+	
 }
+
--- LessonAB23/src/MatrixManipulator.java
+++ LessonAB23/src/MatrixManipulator.java
@@ -12,103 +12,123 @@ public class MatrixManipulator {
    
 	Scanner scan;
 
+	/**
+	 * Constructor to initialize scanner
+	 */
     public MatrixManipulator() {
         scan = new Scanner(System.in);
     }
 
     /**
-     * Put Description here
+     * Returns sum of all array elements
+     * @param data - array to sum elements from
+     * @return sum of elements
      */
-    public int sumOfAllArrayElements( int[][] data ) {
-
-        // declare the sum
-        
-
-        // compute the sum
-        
-    	
-    	
+    public int sumOfAllArrayElements(int[][] data) {
+    	int sum = 0;
+    	for (int i = 0; i < data.length; i++) {
+    		for (int j = 0; j < data[i].length; j++) {
+    			sum += data[i][j];
+    		}
+    	}
         return sum;
     }
 
-
     /**
-     * Put Description here
+     * Return an array of the sums of each row
+     * @param data - array whose elements are to be summed
+     * @return array of sums of each row
      */
-    public int[] sumOfEachRow( int[][] data )
-    {
+    public int[] sumOfEachRow(int[][] data) {
         // declare the row sum array
-        
+        int[] sums = new int[data.length];
         // compute the sums for each row
-        for ( int row = 0; row < data.length; row++ )
-        {
-            // initialize the sum
-
-            // compute the sum for this row
-            
-            
+        for (int row = 0; row < data.length; row++) {
+            for (int j = 0; j < data[row].length; j++) {
+            	sums[row] += data[row][j];
+            }
         }
-
-        return rowSums;
+        return sums;
     }
 
-
     /**
-     * Put Description here
+     * Return an array containing the sum of each column
+     * @param data - array of data to be summed
+     * @return array of sums of each column
      */
-    public int[] sumOfEachColumn( int[][] data )
-    {
+    public int[] sumOfEachColumn(int[][] data) {
         // find the longest row
-        
-
-        // declare the col sum array
-    	
-    	// compute the sum of each column
-
-        return colSums;
+    	int longest = 0;
+        for (int i = 0; i < data.length; i++) {
+        	longest = Math.max(longest, data[i].length);
+        }
+        int[] sums = new int[longest];
+    	for (int i = 0; i < longest; i++) {
+    		for (int j = 0; j < data.length; j++) {
+    			if (data[j].length > i) sums[i] += data[j][i];
+    		}
+    	}
+        return sums;
     }
-
+    
     /**
-     * Put Description here
+     * Checks if the array is a magic square
+     * @param data - array to check
+     * @return whether or not the array is a magic square
      */
-    public boolean isMagic( int[][] data )
-    {
-    	//check if the array is a magic square. This means that it must be square, 
-    	//and that all row sums, all column sums, and the two diagonal-sums 
-    	//must all be equal.
-    	return false;
+    public boolean isMagic(int[][] data) {
+    	int length = data.length;
+    	for (int i = 0; i < data.length; i++) {
+    		if (data[i].length != length) return false;
+    	}
+    	int[] rows = sumOfEachRow(data);
+    	for (int i = 1; i < rows.length; i++) {
+    		if (rows[i] != rows[0]) return false;
+    	}
+    	int[] cols = sumOfEachColumn(data);
+    	for (int i = 1; i < cols.length; i++) {
+    		if (cols[i] != cols[0]) return false;
+    	}
+    	if (rows[0] != cols[0]) return false;
+    	int sum1 = 0, sum2 = 0;
+    	for (int i = 0; i < data.length; i++) {
+    		sum1 += data[i][i];
+    		sum2 += data[data.length-i-1][i];
+    	}
+    	if (sum1 != sum2 || sum1 != rows[0]) return false;
+    	return true;
     }
+    
     /**
-     * Put Description here
+     * Find the max and min elements 
+     * @param data - array whose max/min is desired
+     * @return array containing max and min
      */
-    public int[] maxAndMinElements( int[][] data )
-    {
-        // declare the max and the min
+    public int[] maxAndMinElements(int[][] data) {
         int max = data[0][0];
         int min = data[0][0];
-
-        // compute the sum
-        
-
-       
-        
-
-        int[] result = { max, min };
+        for (int row = 0; row < data.length; row++) {
+        	for (int col = 0; col < data[row].length; col++) {
+        		max = Math.max(max, data[row][col]);
+        		min = Math.min(min, data[row][col]);
+        	}
+        }
+        int[] result = {max, min};
         return result;
     }
 
-
     /**
-     * Put Description here
+     * Compute the largest element in each row
+     * @param data - array whose largest elements are desired
+     * @return array containing largest elements in each row
      */
-    public int[] largestElements( int[][] data )
-    {
-        // declare the largest in row array
+    public int[] largestElements(int[][] data) {
         int[] largestInRow = new int[data.length];
-
-        // find the largest element in each row
- 
-
+        for (int row = 0; row < data.length; row++) {
+        	for (int col = 0; col < data[row].length; col++) {
+        		largestInRow[row] = Math.max(largestInRow[row], data[row][col]);
+        	}
+        }
         return largestInRow;
     }
 

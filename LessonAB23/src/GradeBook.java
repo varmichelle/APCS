d2a8e7e147638237b562cae64e010e1ba74674ee
Michelle Li <michellecarolynclark@gmail.com>
2017-02-18 08:09:57 -0800

    gradebook

--- LessonAB23/src/GradeBook.java
+++ LessonAB23/src/GradeBook.java
@@ -16,11 +16,7 @@ public class GradeBook {
 	 */
 	public GradeBook(String name, int[][] gradesArray) {
 		courseName = name;
-		for (int row = 0; row < gradesArray.length; row++) {
-			for (int col = 0; col < gradesArray[0].length; col++) {
-				grades[row][col] = gradesArray[row][col];
-			}
-		}
+		grades = gradesArray;
 	}
 	
 	/**
@@ -65,12 +61,41 @@ public class GradeBook {
 		return avg;
 	}
 	
+	/**
+	 * Print bar chart grade distribution
+	 */
 	public void outputBarChart() {
-		
+		int[] distribution = new int[11];
+		for (int row = 0; row < grades.length; row++) {
+			for (int col = 0; col < grades[row].length; col++) {
+				distribution[grades[row][col]/10]++;
+			}
+		}
+		for (int i = 0; i <= 9; i++) {
+			System.out.print(i+"0-"+i+"9: ");
+			for (int j = 0; j < distribution[i]; j++) System.out.print("*");
+			System.out.println();
+		}
+		System.out.print("  "+100+": ");
+		for (int j = 0; j < distribution[10]; j++) System.out.print("*");
+		System.out.println();
 	}
 	
+	/**
+	 * Print grades and average for each student
+	 */
 	public void outputGrades() {
-		
+		System.out.println("                Test 1  Test 2  Test 3  Average");
+		for (int i = 0; i < grades.length; i++) {
+			System.out.print("Student");
+			System.out.printf("%3s", (i+1)+"");
+			System.out.print("      ");
+			for (int j = 1; j <= 3; j++) {
+				System.out.printf("%-7s", grades[i][j-1]+"");
+				System.out.print(" ");
+			}
+			System.out.printf("%.2f\n", getAverage(grades[i]));
+		}		
 	}
 	
 	/**
@@ -79,8 +104,8 @@ public class GradeBook {
 	 */
 	public void processGrades() {
 		System.out.println("Lowest grade is " + getMinimum());
-		System.out.println("Highest grade is " + getMaximum());
-		System.out.println("The grades are: ");
+		System.out.println("Highest grade is " + getMaximum()+"\n");
+		System.out.println("The grades are: \n");
 		outputGrades();
 		System.out.println("Overall grade distribution");
 		outputBarChart();
@@ -106,7 +131,7 @@ public class GradeBook {
 	 * Display a message to introduce the course
 	 */
 	public void displayMessage() {
-		System.out.println("Welcome to the gradebook for\nAPCS - AP Computer Science!");
+		System.out.println("Welcome to the gradebook for\nAPCS - AP Computer Science!\n\n");
 	}
 	
 }

fb611aab9ba80a5fefdccd9d0251ba04ed2bacbe
Michelle Li <michellecarolynclark@gmail.com>
2017-02-28 07:50:58 -0800

    initial lessson21

---
+++ LessonA21/src/Digit.java
@@ -0,0 +1,49 @@
+/**
+ * Class Number - represents numbers with different bases
+ * @author Michelle Li
+ * Period 1
+ */
+
+import java.util.*;
+
+public class Digit {
+
+	private int base, value;
+	
+	/**
+	 * Constructor to initialize base to 10 and number to 0
+	 */
+	public Digit() {
+		base = 10;
+		value = 0;
+	}
+	
+	/**
+	 * Constructor to initialize base and value as given
+	 * @param v - number in decimal
+	 * @param b - base to convert v to
+	 */
+	public Digit(int v, int b) {
+		base = b;
+		value = v;
+	}
+	
+	/**
+	 * Convert the digit into a format for printing
+	 */
+	public String toString() {
+		return "" + value;
+	}
+	
+	/**
+	 * Increment the digit by 1
+	 * @return true if there is a carry (false otherwise)
+	 */
+	public boolean increment() {
+		value += 1;
+		value %= base;
+		if (value == 0) return true;
+		else return false;		
+	}
+	
+}
---
+++ LessonA21/src/Number.java
@@ -0,0 +1,72 @@
+/**
+ * Class Number - represents numbers with different bases
+ * @author Michelle Li
+ * Period 1
+ */
+
+import java.util.*;
+
+public class Number {
+
+	private int base, value;
+	private ArrayList<Digit> digits = new ArrayList<Digit>();
+	
+	/**
+	 * Constructor to initialize base to 10 and number to 0
+	 */
+	public Number() {
+		base = 10;
+		value = 0;
+		digits.add(new Digit(0,base));
+	}
+	
+	/**
+	 * Constructor to initialize base and number as given
+	 * @param v - number in decimal
+	 * @param b - base to convert v to
+	 */
+	public Number(int v, int b) {
+		base = b;
+		value = v;
+		int copy = value;
+		int numDigits = 0;
+		int divideBy = 1;
+		while (copy > 0) {
+			copy /= base;
+			numDigits++;
+			divideBy *= base;
+		}
+		copy = value;
+		for (int i = 0; i < numDigits; i++) {
+			digits.add(new Digit (copy / divideBy, base));
+			copy %= divideBy;
+			divideBy /= base;
+		}
+	}
+	
+	/**
+	 * Convert the number into a format for printing
+	 */
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < digits.size(); i++) {
+			s += digits.get(i);
+		}
+		return s;
+	}
+	
+	/**
+	 * Increment the number by 1
+	 * @return true if there is a carry (false otherwise)
+	 */
+	public boolean increment() {
+		boolean carry = false;
+		for (int i = digits.size() - 1; i >= 0; i--) {
+			if (! digits.get(i).increment()) break;
+			carry = true;
+			if (i == 0) digits.add(0, new Digit(1,base));
+		}
+		return carry;
+	}
+	
+}
